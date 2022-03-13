0ad7dfa7ba6adfce0a843f8ef0b5f86fa85f17bc
yanuabialwi <zhalxdev@gmail.com>
2022-03-12 21:18:49 -0500

    Create wpac.js

---
+++ wpac.js
@@ -0,0 +1,12 @@
+wpac_init = window.wpac_init || [];
+wpac_init.push({widget: 'Rating', id: 31267, el: 'wpac-rating-custom',
+html: '{{=it.stars}} {{?it.count>0}} {{=it.count}} {{=it.votes}}{{?}}'});
+(function() {
+    if ('WIDGETPACK_LOADED' in window) return;
+    WIDGETPACK_LOADED = true;
+    var mc = document.createElement('script');
+    mc.type = 'text/javascript';
+    mc.async = true;
+    mc.src = 'https://cdn.widgetpack.com/widget.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(mc, s.nextSibling);
+})();
