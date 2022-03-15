9cdf6ea81bfde31728e93f338314fd23b2c270fb
yanuabialwi <zhalxdev@gmail.com>
2022-03-15 18:12:49 +0700

    Create dsqcnt.js

---
+++ dsqcnt.js
@@ -0,0 +1,10 @@
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = &#39;catatandroid&#39;;
+ 
+    /* * * DON&#39;T EDIT BELOW THIS LINE * * */
+    (function () {
+        var s = document.createElement(&#39;script&#39;); s.async = true;
+        s.type = &#39;text/javascript&#39;;
+        s.src = &#39;//&#39; + disqus_shortname + &#39;.disqus.com/count.js&#39;;
+        (document.getElementsByTagName(&#39;HEAD&#39;)[0] || document.getElementsByTagName(&#39;BODY&#39;)[0]).appendChild(s);
+    }());
