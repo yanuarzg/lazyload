ac6f22ba60c4cb3cbb8f5be6c92a0a646f4f6102
yanuabialwi <zhalxdev@gmail.com>
2022-03-19 16:43:10 +0000

    Update recentdsq.js

--- recentdsq.js
+++ recentdsq.js
@@ -1,1 +1,1 @@
-document.write("<scr" + "ipt type=\"text/javascript\" src=\"https://themeindie.disqus.com/recent_comments_widget.js?num_items=18&hide_mods=0&hide_avatars=1&avatar_size=32&excerpt_length=120\"></scr" + "ipt>");
+document.write("<scr" + "ipt type=\"text/javascript\" src=\"https://catatandroid.disqus.com/recent_comments_widget.js?num_items=18&hide_mods=0&hide_avatars=1&avatar_size=32&excerpt_length=120\"></scr" + "ipt>");
