09757a369af23876a0e7831353716cc27588f0e4
yanuabialwi <zhalxdev@gmail.com>
2022-03-19 18:24:28 +0000

    Update recentdsq.js

--- recentdsq.js
+++ recentdsq.js
@@ -1,1 +1,1 @@
-document.write("<scr" + "ipt type=\"text/javascript\" src=\"https://catatandroid.disqus.com/recent_comments_widget.js?num_items=18&hide_mods=0&hide_avatars=1&avatar_size=32&excerpt_length=120\"></scr" + "ipt>");
+window.jQuery || document.write('<script src="https://catatandroid.disqus.com/recent_comments_widget.js?num_items=18&hide_mods=0&hide_avatars=1&avatar_size=32&excerpt_length=120"><\/script>');
