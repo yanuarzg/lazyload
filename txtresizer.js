55e0efa52266de5899fab3ca855566fce732bc23
yanuabialwi <zhalxdev@gmail.com>
2022-03-23 15:23:08 +0700

    Create txtresizer.js

---
+++ txtresizer.js
@@ -0,0 +1,18 @@
+$(function(){
+$('input').click(function(){
+var ourText = $('.post-body');
+var currFontSize = ourText.css('fontSize');
+var finalNum = parseFloat(currFontSize, 10);
+var stringEnding = currFontSize.slice(-2);
+if(this.id == 'tlarge') {
+finalNum *= 1.1;
+}
+else if (this.id == 'tsmall'){
+finalNum /=1.1;
+}
+else if (this.id == 'treset'){
+finalNum =16;
+}
+ourText.animate({fontSize: finalNum + stringEnding},500);
+});
+});
