528fa9783b1065a7860b8a6440d4836e09a0dd66
yanuabialwi <zhalxdev@gmail.com>
2022-05-22 04:05:23 +0700

    Update and rename tmago.js to tmgo.js

---
+++ tmgo.js
@@ -0,0 +1,1 @@
+function timeAgo(o){var t=(new Date).getTime()-o.getTime(),e=Math.floor(t/1e3),r=Math.floor(e/60),a=Math.floor(r/60),g=Math.floor(a/24),h=Math.floor(g/30),n=Math.floor(h/12);return 0===t?"Just now":e<60?e+" seconds Ago":r<60?r+" minutes Ago":a<24?a+" hours Ago":g<30?g+" days Ago":h<12?h+" months":n+" years Ago"}
