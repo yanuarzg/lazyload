d94d4f8d50e408761fb26d5d692cebbaa665a98d
yanuabialwi <zhalxdev@gmail.com>
2022-06-03 14:09:14 +0700

    Update and rename lzall2.js to lzall3.js

---
+++ lzall3.js
@@ -0,0 +1,12 @@
+/*! yang ngintip */
+jQuery.getScript("//cdn.firebase.com/js/client/2.3.2/firebase.js").done(function(){
+$.each($(".post-view[data-id]"),function(e,a){var i=$(a).parent().find("#postviews").addClass("view-load"),t=new Firebase("https://ctd-rating-default-rtdb.firebaseio.com/pages/id/"+$(a).attr("data-id"));t.once("value",function(e){var o=e.val(),d=!1;null==o&&((o={}).value=0,o.url=window.location.href,o.id=$(a).attr("data-id"),d=!0),i.removeClass("view-load").text(o.value),o.value++,"/"!=window.location.pathname&&(d?t.set(o):t.child("value").set(o.value))})})});
+/*! dsq */
+var disqus_shortname = 'catatandroid';
+(function () {
+   var s = document.createElement('script');
+   s.async = true;
+   s.type = 'text/javascript';
+   s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+   (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
