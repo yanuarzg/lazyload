6395bd39897e903555318dfa855faf84313b671b
yanuabialwi <zhalxdev@gmail.com>
2022-10-11 15:37:33 +0700

    Update and rename rc-cmnt.js to rc-cmnt-btn.js

---
+++ rc-cmnt-btn.js
@@ -0,0 +1,4 @@
+$(function(){$(".notif-show").on("click",function(){$("#disqus-notif").addClass("active").focus()});$(".close-1").on("click",function(){$("#disqus-notif").removeClass("active")})});
+$(function(){$(".notif-show").on("click",function(){$("#overlay-1").addClass("active").focus()});$(".close-1").on("click",function(){$("#overlay-1").removeClass("active")})});
+$("#RecentComments a").filter(function(){return this.hostname&&this.hostname!==location.hostname}).attr('rel', 'nofollow noopener').attr('target', '_blank');
+$(function(){$(".notif-show").on("click",function(){$("dsq-widget-list").addClass("active").focus()});$(".close-1").on("click",function(){$("dsq-widget-list").removeClass("active")})});
