a9a4736b685955abe5b81d0c915159e316dd0655
yanuabialwi <zhalxdev@gmail.com>
2022-10-14 22:32:23 +0700

    Create rc-cmnt-btn2.js

--- rc-cmnt-btn2.js
+++ rc-cmnt-btn2.js
@@ -1,3 +1,4 @@
+$(function() {postscribe('#RecentComments', '<script src=https://catatandroid.disqus.com/recent_comments_widget.js?num_items=15&hide_mods=0&hide_avatars=1&avatar_size=32&excerpt_length=100><\/script>');});
 $(function(){$(".notif-show").on("click",function(){$("#disqus-notif").addClass("active").focus()});$(".close-1").on("click",function(){$("#disqus-notif").removeClass("active")})});
 $(function(){$(".notif-show").on("click",function(){$("#overlay-1").addClass("active").focus()});$(".close-1").on("click",function(){$("#overlay-1").removeClass("active")})});
 $("#RecentComments a").filter(function(){return this.hostname&&this.hostname!==location.hostname}).attr('rel', 'nofollow noopener').attr('target', '_blank');
